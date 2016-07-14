b0eb2887814d7f1ecbe554d80761c0e63866265e
LuuHoang <luugu196@gmail.com>
2016-07-14 15:39:27 +0000

    commit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/home.html'); 
+});
+app.get('/whoami', function(req, res) {
     var header = req.headers;
     var ip = header['x-forwarded-for'],
         langArr = header['accept-language'].split(/\;|\,/),
